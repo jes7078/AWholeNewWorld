0d569ae330614f5836968714a5d8df3acd2978d8
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2019-12-11 12:32:19 -0500

    first version

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AWholeNewWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
